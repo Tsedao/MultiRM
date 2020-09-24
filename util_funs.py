2bac57a8f4bb6f2d74024fef23e35f8095d1974e
Tsedao <34228897+Tsedao@users.noreply.github.com>
2020-09-24 14:07:57 +0800

    Update util_funs.py

--- util_funs.py
+++ util_funs.py
@@ -233,7 +233,7 @@ def visualize(raw_seq,weights,RMs):
         end = np.min([50*(k+1),len(raw_seq)])
         cutted_seqs = raw_seq[start:end]
         # 58 characters
-        print('*'*24+'%3d-%3d bp' %(start+1,end) + '*'*23)
+        print('*'*24+'%3d-%3d nt' %(start+1,end) + '*'*23)
         print('%-7s'%('Origin')+cutted_seqs)
         for i in range(len(RMs)):
             weight = weights[i,:]
